44d1ac5a17cdbca8e4858140fc9133463f84b074
Daniel <daniel-mcintyre@hotmail.com>
2022-11-16 16:14:58 -0500

    created AddTask form component and imported it in the top app level file

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
     <Header title="Task Tracker"/>
+    <AddTask/>
     <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks"/>
   </div>
 </template>
@@ -9,12 +10,14 @@
 
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 
 export default {
   name: 'App',
   components: {
     Header,
-    Tasks
+    Tasks, 
+    AddTask
   },
   data() {
     return {
@@ -99,3 +102,4 @@ body {
   width: 100%;
 }
 </style>
+
